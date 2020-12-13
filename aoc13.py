584660dab1af29944e13d6883ea448d6e133bd89
remowxdx <papik.meli@gmail.com>
2020-12-13 09:49:36 +0100

    Finished Day 13 Part 1.

--- aoc13.py
+++ aoc13.py
@@ -4,7 +4,7 @@ from aoc import *
 
 pd = Debug(True)
 DAY = 13
-SOLVED_1 = False
+SOLVED_1 = True
 SOLVED_2 = False
 
 def get_input(filename):
@@ -12,23 +12,57 @@ def get_input(filename):
         lines = f.read()
     return lines.splitlines()
 
+def get_lines_in_service(lines):
+    service = []
+    for line in lines.split(','):
+        if line == 'x':
+            continue
+        service.append(int(line))
+    return service
+
 def test1(data):
-    return 0
+    after = int(data[0])
+    lines = get_lines_in_service(data[1])
+    next_departure = {}
+    earliest_line = -1
+    earliest_departure = after + lines[0]
+    for line in lines:
+        departure = ((after // line) + 1) * line
+        next_departure[line] = departure
+        if departure < earliest_departure:
+            earliest_departure = departure
+            earliest_line = line
+
+    return (earliest_departure - after) * earliest_line
 
 def test2(data):
     return 0
 
 def part1(data):
-    return None
+    after = int(data[0])
+    lines = get_lines_in_service(data[1])
+    next_departure = {}
+    earliest_line = -1
+    earliest_departure = after + lines[0]
+    for line in lines:
+        departure = ((after // line) + 1) * line
+        next_departure[line] = departure
+        if departure < earliest_departure:
+            earliest_departure = departure
+            earliest_line = line
+
+    return (earliest_departure - after) * earliest_line
 
 def part2(data):
     return None
 
 if __name__ == '__main__':
 
-    test_input_1 = [1,2,3]
+    test_input_1 = '''939
+7,13,x,x,59,x,31,19
+'''.splitlines()
     print('Test Part 1:')
-    test_eq('Test 1.1', test1, 42, test_input_1)
+    test_eq('Test 1.1', test1, 295, test_input_1)
     print()
 
     test_input_2 = [4,5,6]

971acf777d0366c2c36253bb264ee666ecb8244d
remowxdx <papik.meli@gmail.com>
2020-12-19 10:18:54 +0100

    Finished Day 19, Part 2.

--- aoc19.py
+++ aoc19.py
@@ -5,7 +5,7 @@ from aoc import *
 pd = Debug(True)
 DAY = 19
 SOLVED_1 = True
-SOLVED_2 = False
+SOLVED_2 = True
 
 def get_input(filename):
     with open(filename, 'r') as f:
@@ -41,6 +41,8 @@ def parse_input(data):
     return rules, messages
 
 def check(message, rule, rules):
+    if len(message) == 0:
+        return []
     if type(rule) != tuple:
         return check(message, rules[rule], rules)
     # print('Check', message, rule)
@@ -66,6 +68,9 @@ def check(message, rule, rules):
         r.extend(check(message, rule[2], rules))
         return r
 
+def update_rules(rules):
+    rules['8'] = ('or', '42', ('and', '42', '8'))
+    rules['11'] = ('or', ('and', '42', '31'), ('and', '42', '11', '31'))
 
 def test1(data):
     rules, messages = parse_input(data)
@@ -74,16 +79,46 @@ def test1(data):
     # print(rules)
     for m in messages:
         r = check(m, '0', rules)
+        if len(r) == 0:
+            continue
         if len(r) == 1 and r[0] == len(m):
             count += 1
             valid.append(m)
+            continue
     print(valid)
     return count
 
 def test2(data):
-    return 0
+    rules, messages = parse_input(data)
+    update_rules(rules)
+    valid = []
+    count = 0
+    print(rules)
+    for m in messages:
+        r = check(m, '0', rules)
+        if len(r) == 0:
+            continue
+        if len(r) == 1 and r[0] == len(m):
+            count += 1
+            valid.append(m)
+            continue
+        if len(r) == 1 and r[0] < len(m):
+            continue
+        if len(r) > 1:
+            for l in r:
+                if l == len(m):
+                    count += 1
+                    valid.append(m)
+                    break
+            continue
+        print(m)
+        print(r, len(m))
+        raise ValueError('???')
+    print(valid)
+    return count
 
 def part1(data):
+    return None
     rules, messages = parse_input(data)
     valid = []
     count = 0
@@ -97,7 +132,33 @@ def part1(data):
     return count
 
 def part2(data):
-    return None
+    rules, messages = parse_input(data)
+    update_rules(rules)
+    valid = []
+    count = 0
+    print(rules)
+    for m in messages:
+        r = check(m, '0', rules)
+        if len(r) == 0:
+            continue
+        if len(r) == 1 and r[0] == len(m):
+            count += 1
+            valid.append(m)
+            continue
+        if len(r) == 1 and r[0] < len(m):
+            continue
+        if len(r) > 1:
+            for l in r:
+                if l == len(m):
+                    count += 1
+                    valid.append(m)
+                    break
+            continue
+        print(m)
+        print(r, len(m))
+        raise ValueError('???')
+    print(valid)
+    return count
 
 if __name__ == '__main__':
 
@@ -118,9 +179,56 @@ aaaabbb
     test_eq('Test 1.1', test1, 2, test_input_1)
     print()
 
-    test_input_2 = [4,5,6]
+    test_input_2 = '''42: 9 14 | 10 1
+9: 14 27 | 1 26
+10: 23 14 | 28 1
+1: "a"
+11: 42 31
+5: 1 14 | 15 1
+19: 14 1 | 14 14
+12: 24 14 | 19 1
+16: 15 1 | 14 14
+31: 14 17 | 1 13
+6: 14 14 | 1 14
+2: 1 24 | 14 4
+0: 8 11
+13: 14 3 | 1 12
+15: 1 | 14
+17: 14 2 | 1 7
+23: 25 1 | 22 14
+28: 16 1
+4: 1 1
+20: 14 14 | 1 15
+3: 5 14 | 16 1
+27: 1 6 | 14 18
+14: "b"
+21: 14 1 | 1 14
+25: 1 1 | 1 14
+22: 14 14
+8: 42
+26: 14 22 | 1 20
+18: 15 15
+7: 14 5 | 1 21
+24: 14 1
+
+abbbbbabbbaaaababbaabbbbabababbbabbbbbbabaaaa
+bbabbbbaabaabba
+babbbbaabbbbbabbbbbbaabaaabaaa
+aaabbbbbbaaaabaababaabababbabaaabbababababaaa
+bbbbbbbaaaabbbbaaabbabaaa
+bbbababbbbaaaaaaaabbababaaababaabab
+ababaaaaaabaaab
+ababaaaaabbbaba
+baabbaaaabbaaaababbaababb
+abbbbabbbbaaaababbbbbbaaaababb
+aaaaabbaabaaaaababaa
+aaaabbaaaabbaaa
+aaaabbaabbaaaaaaabbbabbbaaabbaabaaa
+babaaabbbaaabaababbaabababaaab
+aabbbbbaabbbaaaaaabbbbbababaaaaabbaaabba
+'''.splitlines()
     print('Test Part 2:')
-    test_eq('Test 2.1', test2, 42, test_input_2)
+    test_eq('Test 2.1', test2, 12, test_input_2)
     print()
 
     data = get_input(f'input{DAY}')

19ec7a4eb9afd477dac4c3e01c56f86bc5b0da2c
remowxdx <papik.meli@gmail.com>
2020-12-24 01:15:27 +0100

    Finished Day 23, Part 2.

--- aoc23.py
+++ aoc23.py
@@ -5,44 +5,124 @@ from aoc import *
 pd = Debug(True)
 DAY = 23
 SOLVED_1 = True
-SOLVED_2 = False
+SOLVED_2 = True
+
+class Cups2:
+    def __init__(self, cups):
+        self.cups = {cups[0]: None}
+        self.current = cups[0]
+        self.picks = {}
+        p = cups[0]
+        for c in cups[1:]:
+            self.cups[p] = c
+            p = c
+        self.cups[p] = self.current
+
+    def pick(self):
+        self.picks = self.cups[self.current]
+        self.cups[self.current] = self.cups[self.cups[self.cups[self.picks]]]
+        self.cups[self.cups[self.cups[self.picks]]] = None
+
+    def insert(self):
+        destination_num = self.current
+        while True:
+            destination_num -= 1
+            if destination_num <= 0:
+                destination_num = len(self.cups)
+
+            if destination_num == self.picks or destination_num == self.cups[self.picks] or destination_num == self.cups[self.cups[self.picks]]:
+                continue
+
+            else:
+                break
+
+        n = self.cups[destination_num]
+        self.cups[destination_num] = self.picks
+        self.cups[self.cups[self.cups[self.picks]]] = n
+        self.picks = None
+        self.current = self.cups[self.current]
+
+
+    def move(self):
+        self.pick()
+        self.insert()
+
+    def ret(self):
+        r = ''
+        c = self.cups[1]
+        while c != 1:
+            r += str(c)
+            c = self.cups[c]
+        return r
+
+    def ret_2(self):
+        n1 = self.cups[1]
+        n2 = self.cups[n1]
+        print(n1, n2)
+        return n1 * n2
+
 
 class Cup:
-    def __init__(self, num, next):
+    def __init__(self, num):
         self.num = num
-        self.next = next
+        self.next = None
 
 
 class Cups:
     def __init__(self, cups):
-        self.cups = []
+        self.max = 0
+        self.current = Cup(cups[0])
+        c = self.current
+        if c.num == 1:
+            self.one = c
         self.picks = None
-        for n in list(cups[0].strip()):
-            self.cups.append(Cup(int(n), None))
-        for i, cup in enumerate(self.cups):
-            cup.next = self.cups[(i + 1) % len(self.cups)]
-        self.current = self.cups[0]
+        if c.num == 6:
+            self.six = c
+        for n in cups[1:]:
+            if n > self.max:
+                self.max = n
+            c.next = Cup(n)
+            c = c.next
+            if c.num == 1:
+                self.one = c
+            if c.num == 6:
+                self.six = c
+            if c.num == self.current.num - 1:
+                self.prev = c
+        c.next = self.current
+        print('Starting...')
+        print(self)
 
     def pick(self):
         self.picks = self.current.next
         self.current.next = self.picks.next.next.next
         self.picks.next.next.next = None
-        print(self)
+        # print(self)
 
     def insert(self):
-        destination_num = self.current.num - 1
-        cursor = self.current
+        destination_num = self.current.num
+        if self.six == self.picks or self.six == self.picks.next or self.six == self.picks.next.next:
+            cursor = self.current
+        else:
+            cursor = self.six
         while True:
-            for r in range(7):
+            destination_num -= 1
+            if destination_num <= 0:
+                destination_num = self.max
+
+            if destination_num == self.picks.num or destination_num == self.picks.next.num or destination_num == self.picks.next.next.num:
+                continue
+
+            for r in range(self.max - 3):
                 if cursor.num == destination_num:
                     n = cursor.next
                     cursor.next = self.picks
                     self.picks.next.next.next = n
                     self.picks = None
                     self.current = self.current.next
+                    print(self.one.next.num, self.one.next.next.num)
                     return None
                 cursor = cursor.next
-            destination_num = (destination_num - 1) % 10
             
     def move(self):
         self.pick()
@@ -52,16 +132,24 @@ class Cups:
         c = self.current
         cups = [str(c.num)]
         c = c.next
+        count = 0
         while c != self.current:
+            if count > 100:
+                break
             cups.append(str(c.num))
             c = c.next
+            count += 1
         str_cups = 'Cups: ' + ' '.join(cups)
 
         c = self.picks
         picks = []
+        count = 0
         while c != None:
+            if count > 100:
+                break
             picks.append(str(c.num))
             c = c.next
+            count += 1
         str_picks = 'Picks: ' + ' '.join(picks)
 
         return str_cups + '\n' + str_picks
@@ -77,6 +165,13 @@ class Cups:
             c = c.next
         return r
 
+    def ret_2(self):
+        c = self.current
+        while c.num != 1:
+            c = c.next
+
+        print(c.next.num * c.next.next.num)
+
 
 def get_input(filename):
     with open(filename, 'r') as f:
@@ -85,26 +180,46 @@ def get_input(filename):
 
 
 def test1(data):
-    cups = Cups(data)
+    cups_list = [int(n) for n in data[0]]
+    cups = Cups2(cups_list)
     for i in range(10):
-        print(cups)
+        # print(cups)
         cups.move()
-    print(cups)
+    # print(cups)
     return cups.ret()
 
 def test2(data):
-    return 0
+    cups_list = [int(n) for n in data[0]]
+    cups_list.extend(list(range(10, 1_000_001)))
+    cups = Cups2(cups_list)
+    for i in range(10_000_000):
+        # print(cups)
+        cups.move()
+        if i % 1_000_000 == 0:
+            print(i)
+    print(cups)
+    return cups.ret_2()
 
 def part1(data):
-    cups = Cups(data)
+    cups_list = [int(n) for n in data[0]]
+    cups = Cups2(cups_list)
     for i in range(100):
-        print(cups)
+        # print(cups)
         cups.move()
-    print(cups)
+    # print(cups)
     return cups.ret()
 
 def part2(data):
-    return None
+    cups_list = [int(n) for n in data[0]]
+    cups_list.extend(list(range(10, 1_000_001)))
+    cups = Cups2(cups_list)
+    for i in range(10_000_000):
+        # print(cups)
+        cups.move()
+        if i % 1_000_000 == 0:
+            print(i)
+    print(cups)
+    return cups.ret_2()
 
 if __name__ == '__main__':
 
@@ -116,7 +231,7 @@ if __name__ == '__main__':
 
     test_input_2 = [4,5,6]
     print('Test Part 2:')
-    test_eq('Test 2.1', test2, 42, test_input_1)
+    test_eq('Test 2.1', test2, 149245887792, test_input_1)
     print()
 
     data = get_input(f'input{DAY}')

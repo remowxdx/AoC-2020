ebc82a2bd1968d75b10c236d824c01cf6bc78d1a
remowxdx <papik.meli@gmail.com>
2020-12-25 10:32:59 +0100

    Finished Day 25, Part1.

--- aoc25.py
+++ aoc25.py
@@ -4,7 +4,7 @@ from aoc import *
 
 pd = Debug(True)
 DAY = 25
-SOLVED_1 = False
+SOLVED_1 = True
 SOLVED_2 = False
 
 def get_input(filename):
@@ -12,23 +12,56 @@ def get_input(filename):
         lines = f.read()
     return lines.splitlines()
 
+
+def loop_size(subject_number, mod, pk):
+    value = 1
+    ls = 0
+    while value != pk:
+        value = (value * subject_number) % mod
+        ls += 1
+    print(ls)
+    return ls
+
+def encryption_key(subject_number, loop_size, mod):
+    value = 1
+    i = 0
+    for i in range(loop_size):
+        value = (value * subject_number) % mod
+
+    return value
+
 def test1(data):
-    return 0
+    subject_number = 7
+    mod = 20201227
+    card_pk = int(data[0])
+    door_pk = int(data[1])
+    card_ls = loop_size(subject_number, mod, card_pk)
+    # card_ls = loop_size(subject_number, mod, card_pk)
+    r = encryption_key(door_pk, card_ls, mod)
+    return r
 
 def test2(data):
     return 0
 
 def part1(data):
-    return None
+    subject_number = 7
+    mod = 20201227
+    card_pk = int(data[0])
+    door_pk = int(data[1])
+    card_ls = loop_size(subject_number, mod, card_pk)
+    # card_ls = loop_size(subject_number, mod, card_pk)
+    r = encryption_key(door_pk, card_ls, mod)
+    return r
+
 
 def part2(data):
     return None
 
 if __name__ == '__main__':
 
-    test_input_1 = [1,2,3]
+    test_input_1 = ['5764801', '17807724']
     print('Test Part 1:')
-    test_eq('Test 1.1', test1, 42, test_input_1)
+    test_eq('Test 1.1', test1, 14897079, test_input_1)
     print()
 
     test_input_2 = [4,5,6]
